9f065f7c94323e1e1fc2a4fb9fd7e2388fe03bed
GeekGhc <ghcgavin@sina.com>
2019-11-06 00:34:16 +0800

    gorm

--- models/models.go
+++ models/models.go
@@ -1,6 +1,11 @@
 package models
 
-import "github.com/jinzhu/gorm"
+import (
+	"fmt"
+	"gin-server-api/pkg/setting"
+	"github.com/jinzhu/gorm"
+	"log"
+)
 
 var db *gorm.DB
 
@@ -12,10 +17,23 @@ type Model struct {
 }
 
 // initializes the database instance
-func Setup(){
+func Setup() {
+	var err error
+	db, err = gorm.Open(setting.DatabaseSetting.Type, fmt.Sprint("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True&loc=Local",
+		setting.DatabaseSetting.User,
+		setting.DatabaseSetting.Password,
+		setting.DatabaseSetting.Host,
+		setting.DatabaseSetting.Name))
+
+	if err != nil {
+		log.Fatalf("models.Setup err: %v", err)
+	}
+
+	gorm.DefaultTableNameHandler = func(db *gorm.DB, defaultTableName string) string {
+		return setting.DatabaseSetting.TablePrefix + defaultTableName
+	}
 
 }
-
 
 // CloseDB closes database connection (unnecessary)
 func CloseDB() {
