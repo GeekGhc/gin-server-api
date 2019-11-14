ea7dbadcff2ebf110879315d87465f75a72a4452
GeekGhc <ghcgavin@sina.com>
2019-11-15 00:23:48 +0800

    encode md5

---
+++ pkg/util/md5.go
@@ -0,0 +1,14 @@
+package util
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+)
+
+// md5 encrypting
+func EncodeMD5(value string) string{
+	m := md5.New()
+	m.Write([]byte(value))
+
+	return hex.EncodeToString(m.Sum(nil))
+}
