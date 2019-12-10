7a8bc72ac6d3197964fbb66a59587fd9e9ce6cc7
GeekGhc <ghcgavin@sina.com>
2019-12-11 00:01:32 +0800

    main

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"gin-server-api/models"
 	"gin-server-api/pkg/logger"
 	"gin-server-api/pkg/setting"
 	"gin-server-api/pkg/util"
 	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
 )
 
 func init() {
@@ -19,6 +22,21 @@ func main() {
 	gin.SetMode(setting.ServerSetting.RunMode)
 
 	gin.ForceConsoleColor()
+
+	readTimeout := setting.ServerSetting.ReadTimeout
+	writeTimeout := setting.ServerSetting.WriteTimeout
+	endPoint := fmt.Sprintf(":%d",setting.ServerSetting.HttpPort)
+	maxHeaderBytes := 1 << 20
+
+	server := &http.Server{
+		Addr:endPoint,
+		ReadTimeout:readTimeout,
+		WriteTimeout:writeTimeout,
+		MaxHeaderBytes:maxHeaderBytes,
+	}
+	log.Printf("[info] start http server listening %s",endPoint)
+	server.ListenAndServe()
+
 	router := gin.Default()
 
 	router.GET("/ping", func(c *gin.Context) {
