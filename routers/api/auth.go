3e258baa3c0fb40bfe16f2dea292c9e1507313bd
GeekGhc <ghcgavin@sina.com>
2019-12-18 18:00:43 +0800

    auth api

--- routers/api/auth.go
+++ routers/api/auth.go
@@ -3,6 +3,8 @@ package api
 import (
 	"gin-server-api/pkg/app"
 	"gin-server-api/pkg/e"
+	"gin-server-api/pkg/util"
+	"gin-server-api/service"
 	"github.com/astaxie/beego/validation"
 	"github.com/gin-gonic/gin"
 	"net/http"
@@ -23,13 +25,32 @@ func Auth(c *gin.Context) {
 	a := auth{Username: username, Password: password}
 	ok, _ := valid.Valid(&a)
 
+	//字段校验
 	if !ok {
 		app.MarkErrors(valid.Errors)
 		appG.Response(http.StatusBadRequest, e.INVALID_PARAMS, nil)
 		return
 	}
 
+	authService := service.Auth{Username:username,Password:password}
+	isExist,err := authService.Check()
+	if err != nil{
+		appG.Response(http.StatusInternalServerError,e.ERROR_AUTH_CHECK_TOKEN_FAIL,nil)
+		return
+	}
+
+	if !isExist{
+		appG.Response(http.StatusUnauthorized,e.ERROR_AUTH_TOKEN,nil)
+		return
+	}
+
+	token,err := util.GenerateToken(username,password)
+	if err != nil{
+		appG.Response(http.StatusInternalServerError, e.ERROR_AUTH_TOKEN, nil)
+		return
+	}
+
 	appG.Response(http.StatusOK, e.SUCCESS, map[string]string{
-		"token": "xxxx",
+		"token": token,
 	})
 }
---
+++ service/auth.go
@@ -0,0 +1,13 @@
+package service
+
+import "gin-server-api/models"
+
+type Auth struct {
+	Username string
+	Password string
+}
+
+//用户是否登录
+func (a *Auth) Check() (bool, error) {
+	return models.CheckAuth(a.Username, a.Password)
+}
