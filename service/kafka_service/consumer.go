11b5cef3b1ab2ea1a9c28e002b3a2d24db668d0d
GeekGhc <ghcgavin@sina.com>
2020-01-12 15:26:19 +0800

    kafka consumer

--- service/kafka_service/consumer.go
+++ service/kafka_service/consumer.go
@@ -1,1 +1,45 @@
 package kafka_service
+
+import (
+	"fmt"
+	"gin-server-api/pkg/setting"
+	"github.com/Shopify/sarama"
+	"sync"
+)
+
+var (
+	wg sync.WaitGroup
+)
+
+func Consumer() {
+	consumer, err := sarama.NewConsumer([]string{"127.0.0.1:9092"}, nil)
+	if err != nil {
+		return
+	}
+
+	defaultTopic := setting.KafkaSetting.DefaultTopic
+	//返回主题的所有分区
+	partitionList, err := consumer.Partitions(defaultTopic)
+	if err != nil {
+		panic(err)
+	}
+
+	for partition := range partitionList {
+		//ConsumePartition方法根据主题，分区和给定的偏移量创建创建了相应的分区消费者
+		pc, err := consumer.ConsumePartition(defaultTopic, int32(partition), sarama.OffsetNewest)
+		if err != nil {
+			panic(err)
+		}
+		defer pc.AsyncClose()
+		wg.Add(1)
+		go func(sarama.PartitionConsumer) {
+			defer wg.Done()
+			//Message 返回一个消费类型的只读通道 有代理产生
+			for msg := range pc.Messages() {
+				fmt.Printf("%s---Partition:%d, Offset:%d, Key:%s, Value:%s\n", msg.Topic, msg.Partition, msg.Offset, string(msg.Key), string(msg.Value))
+			}
+		}(pc)
+	}
+	wg.Wait()
+	consumer.Close()
+}
